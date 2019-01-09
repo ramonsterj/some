e433cb50a61b52be5a3f9fded4299f14ecf1f188
ramonsterj <juan@catalystdevs.com>
2019-01-09 11:53:50 +0000

    added echo method to call in use-some

--- src/main/java/uk/org/rspca/MyClass.java
+++ src/main/java/uk/org/rspca/MyClass.java
@@ -4,4 +4,8 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello!!!");
     }
+
+    public String echo(String repeat) {
+        return repeat;
+    }
 }
